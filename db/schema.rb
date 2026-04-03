0a6df1fa95a4b571d4d9ee126be2ee8b50b09870
bishesh-ganesh-shrestha <unikbisheshshrestha12@gmail.com>
2026-04-03 22:05:59 +0545

    add content model with null constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_03_122437) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_03_161828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "contents", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_contents_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "country"
@@ -23,4 +32,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_03_122437) do
     t.string "password_digest"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "contents", "users"
 end
