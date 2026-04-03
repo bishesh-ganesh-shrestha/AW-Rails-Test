bc8c5a6055432a55a6d27e79f099c6ec3c6bcb1e
bishesh-ganesh-shrestha <unikbisheshshrestha12@gmail.com>
2026-04-03 18:35:27 +0545

    add jwt helper module

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,15 @@
+module JsonWebToken
+  SECRET_KEY = ENV.fetch("JWT_SECRET", "secret_key")
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET_KEY)
+  end
+
+  def self.decode(token)
+    decoded = JWT.decode(token, SECRET_KEY)[0]
+    HashWithIndifferentAccess.new(decoded)
+  rescue JWT::DecodeError => e
+    raise StandardError, e.message
+  end
+end
