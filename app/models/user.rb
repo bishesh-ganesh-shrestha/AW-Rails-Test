aa12747a609005b693b97160e3517bbec22c13a4
bishesh-ganesh-shrestha <unikbisheshshrestha12@gmail.com>
2026-04-03 18:24:45 +0545

    add validations in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
